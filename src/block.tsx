932b41ebe61a7772fe6f9dabdab534239d20b9e3
warismee <waris.mee6245@gmail.com>
2025-10-15 21:38:29 +0700

    remove 3d

--- src/block.tsx
+++ src/block.tsx
@@ -42,14 +42,6 @@ type ViewMode = "2d-schematic";
 
 
 
-function LoadingSpinner() {
-  return (
-    <div className="flex items-center justify-center h-full">
-      <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-500"></div>
-      <span className="ml-3 text-gray-600">Loading 3D Scene...</span>
-    </div>
-  );
-}
 
 interface QuestionSummaryProps {
   isValid: boolean;
